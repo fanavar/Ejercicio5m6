c6816b8409f47ee5181e87eeaa2ab39810929813
Fabián <fanavar@alumnos.ubiobio.cl>
2023-08-01 22:21:12 -0400

    repo

---
+++ app/src/main/java/cl/awakelab/ejercicio5m6/data/Repositorio.kt
@@ -0,0 +1,7 @@
+package cl.awakelab.ejercicio5m6.data
+
+import cl.awakelab.ejercicio5m6.data.remote.TerrenoAPI
+
+class Repositorio(private val terrenoAPI: TerrenoAPI) {
+    suspend fun cargarTerreno():
+}
